4c077bbb9b83ed21f5f32caf2eabb45a0fd21ebb
JohntheFish <john@deepsea.co.uk>
2014-05-15 14:12:26 +0100

    Minor changes to work with view.css

--- blocks/theme_switcher/view.php
+++ blocks/theme_switcher/view.php
@@ -1,22 +1,21 @@
-<?php defined('C5_EXECUTE') or die("Access Denied.");
+<?php  defined('C5_EXECUTE') or die("Access Denied.");
 
 $form = Loader::helper('form');
 ?>
 
 <div class="themeSwitcher">
-	<h2><?php echo $title?></h2>
-		
-	<form method="post" action="<?php echo $this->action('switch_theme')?>" id="theme-switcher">
-		<select style="width: 175px; " id="theme" name="theme" class="ccm-input-select">
-			<?php 
-			foreach($themeHandles as $theme){ 
+	<form method="post" action="<?php  echo $this->action('switch_theme')?>" class="theme-switcher">
+		<h2><?php  echo $title?></h2>
+		<select name="theme">
+			<?php
+			foreach($themeHandles as $theme){
 				$themeName = $theme->getThemeName();
 				$themeHandle = $theme->getThemeHandle();
 				$selected = '';
 				if($themeHandle == $selectedTheme) {
 					$selected = ' selected="selected"';
 				}
-				
+
 				echo '<option value="'.$themeHandle.'"'.$selected.'>'.$themeName.'</option>';
 			} ?>
 		</select>
