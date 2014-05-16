949c4ce57cf67ad808f3a1a11118154d23320afa
JohntheFish <john@deepsea.co.uk>
2014-05-16 09:35:13 +0100

    Update view.php

--- blocks/theme_switcher/view.php
+++ blocks/theme_switcher/view.php
@@ -3,19 +3,20 @@
 $form = Loader::helper('form');
 ?>
 
-<div class="themeSwitcher">
+<div class="ccm-ui themeSwitcher">
+	<h2><?php  echo $title?></h2>
+		
 	<form method="post" action="<?php  echo $this->action('switch_theme')?>" class="theme-switcher">
-		<h2><?php  echo $title?></h2>
-		<select name="theme">
-			<?php
-			foreach($themeHandles as $theme){
+		<select name="theme" class="ccm-input-select">
+			<?php  
+			foreach($themeHandles as $theme){ 
 				$themeName = $theme->getThemeName();
 				$themeHandle = $theme->getThemeHandle();
 				$selected = '';
 				if($themeHandle == $selectedTheme) {
 					$selected = ' selected="selected"';
 				}
-
+				
 				echo '<option value="'.$themeHandle.'"'.$selected.'>'.$themeName.'</option>';
 			} ?>
 		</select>
